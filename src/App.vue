0e11b936810967e497f573156a5c9534e62a7cd5
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2017-11-15 00:49:07 -0500

    first commit - file upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <v-toolbar app>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <big>Lead Conversion Evaluator</big>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; MIT Licence</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Decision Support System for Renewable Energy',
+      };
+    },
+  };
+
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <v-layout column align-center>
+      <v-btn
+      href="https://drive.google.com/open?id=1lL6Snzht4stPq1AXnHf-mw5F3Vq_I9Ub"
+      target="blank">
+        {{downloadMsg}}
+        <v-icon right dark>cloud_download</v-icon>
+      </v-btn>
+      <v-container fluid>
+          <v-layout column align-center>
+            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
+            <blockquote>
+              &#8220;First, solve the problem. Then, write the code.&#8221;
+              <footer>
+                <small>
+                  <em>&mdash;John Johnson</em>
+                </small>
+              </footer>
+            </blockquote>
+          </v-layout>
+      </v-container>
+
+         <FileUpload></FileUpload>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import FileUpload from './FileUpload';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      downloadMsg: 'Click Here to Download the template of CSV file',
+    };
+  },
+  methods: {
+  },
+  components: {
+    FileUpload,
+  },
+};
+</script>
+
+<style lang="stylus">
+  @import '../stylus/main'
+</style>
