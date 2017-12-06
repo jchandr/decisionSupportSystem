285eba5148178352e148d6f6bf52b03c9c02ba9e
Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>
2017-12-06 02:34:03 -0500

    fixes
    Signed-off-by: Jeyabalaji Chandrasekaran <jeyabalajic93@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <router-view></router-view>
+    <FileUpload @upload="handleUpload"></FileUpload>
     <v-layout column align-center>
       <v-btn
       href="https://drive.google.com/open?id=1lL6Snzht4stPq1AXnHf-mw5F3Vq_I9Ub"
@@ -8,20 +9,6 @@
         {{downloadMsg}}
         <v-icon right dark>cloud_download</v-icon>
       </v-btn>
-      <!-- <v-container fluid>
-          <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-layout>
-      </v-container> -->
-         <FileUpload @upload="handleUpload"></FileUpload>
     </v-layout>
   </div>
 </template>
